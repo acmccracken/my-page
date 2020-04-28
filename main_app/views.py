41d87207881af4a36c2c86b05bc481f958aca44c
Alan McCracken <acmccracken@gmail.com>
2020-04-28 15:49:24 -0500

    page set up

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,17 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def pages_index(request):
+  return render(request, 'pages/index.html', { 'pages': pages })
+
+
+class Page:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, title, post):
+    self.title = title
+    self.post = post
+    
+pages = [
+    Page('Title', 'This is a temperary string of words'),
+    Page('Also a title', 'Lets have 2')
+]
